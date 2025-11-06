bc2cf4ef00f4df5b5e8e71243e0b5ea14feae28f
kannans8 <subeesh.kannan@roche.com>
2025-11-06 15:59:50 +0530

    Refactor iframe rendering for Gradio component

--- app.py
+++ app.py
@@ -1,27 +1,19 @@
 import streamlit as st
 import requests
 gradio_url = "https://2fe477cc111f555252.gradio.live"
-response = requests.get(gradio_url, timeout= 200)
-if (response.status_code >= 200 and response.status_code < 400):
-    
-    try:
-    
-        st.components.v1.html(
-            f"""
-            <iframe src="{gradio_url}" 
-                    width="100%" 
-                    height="800" 
-                    style="border:none;" 
-                    scrolling="yes">
-            </iframe>
-            """,
-            height=800
-        )
-    except Exception :
-        st.markdown("## Site is down now . Try later")
 
-else :
-    st.markdown("## Site is down now . Try later")
+st.components.v1.html(
+    f"""
+    <iframe src="{gradio_url}" 
+            width="100%" 
+            height="800" 
+            style="border:none;" 
+            scrolling="yes">
+    </iframe>
+    """,
+    height=800
+)
+
 
 
     
