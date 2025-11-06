a76984157d081c9da83d3d65d20708c1092ecf9f
kannans8 <subeesh.kannan@roche.com>
2025-11-06 17:55:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 st.set_page_config(layout="wide")
-gradio_url = "https://f1e4ed93361006f676.gradio.live"
+gradio_url = "https://d8f785e4a6f8249882.gradio.live"
 st.components.v1.html(
     f"""
     <iframe src="{gradio_url}" 
