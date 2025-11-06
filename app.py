4b33503c30a9e85c6984dff53771192e221fede7
kannans8 <subeesh.kannan@roche.com>
2025-11-06 15:57:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 import streamlit as st
-
-try:
+import requests
+gradio_url = "https://2fe477cc111f555252.gradio.live"
+response = requests.get(gradio_url, timeout=timeout)
+if (response.status_code >= 200 and response.status_code < 400):
     
-    gradio_url = "https://2fe477cc111f555252.gradio.live"
-    st.components.v1.html(
-        f"""
-        <iframe src="{gradio_url}" 
-                width="100%" 
-                height="800" 
-                style="border:none;" 
-                scrolling="yes">
-        </iframe>
-        """,
-        height=800
-    )
-except Exception :
+    try:
+    
+        st.components.v1.html(
+            f"""
+            <iframe src="{gradio_url}" 
+                    width="100%" 
+                    height="800" 
+                    style="border:none;" 
+                    scrolling="yes">
+            </iframe>
+            """,
+            height=800
+        )
+    except Exception :
+        st.markdown("## Site is down now . Try later")
+
+else :
     st.markdown("## Site is down now . Try later")
+
 
     
 
