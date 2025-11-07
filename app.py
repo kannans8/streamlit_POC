e3ff7590787d7f4efdc4ff8f34b064ed2130eedf
kannans8 <subeesh.kannan@roche.com>
2025-11-07 11:05:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
 import streamlit as st
 import requests
 st.set_page_config(layout="wide")
+
+base64_img = get_base64_of_bin_file("C:/Users/KANNANS8/Downloads/Subeesh/Keyword search/Picture1.png")
+
+st.markdown(
+    """
+    <style>
+
+        .stApp {
+            background: url("data:image/jpg;base64,"""+base64_img+""" ") no-repeat center center fixed;
+            background-size: cover;
+            background-position: center 25px;
+        }
+
+    </style>
+
+    <div class="title">Crawler AI_V2</div>
+    <div style="position: fixed; bottom: 10px; left: 100px; color: white; font-size: 20px;">
+        Gemini API used
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+
+
+
 gradio_url = "https://f9fe2e123b4a9f88bd.gradio.live/"
 st.components.v1.html(
     f"""
